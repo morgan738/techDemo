f123ccdd7cc63c66f3d2edbf6d69107462b8a0fe
morgan738 <37817281+morgan738@users.noreply.github.com>
2021-11-15 13:10:17 -0500

    added slide movement to player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -24,7 +24,8 @@ public class PlayerMovement : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    void Awake(){
+    void Awake()
+    {
         rigidbody2D = transform.GetComponent<Rigidbody2D>();
         boxCollider2D = transform.GetComponent<BoxCollider2D>();
     }
@@ -37,68 +38,125 @@ public class PlayerMovement : MonoBehaviour
 
         //checks to see if character is on the gorund. if they are, they have the ability to jump
         //Input.GetKeyDown(KeyCode.Space)
-        if((isGrounded() && Input.GetKeyDown(KeyCode.W)) || (isGrounded() && Input.GetKeyDown(KeyCode.UpArrow) )){
+        if ((isGrounded() && Input.GetKeyDown(KeyCode.W)) || (isGrounded() && Input.GetKeyDown(KeyCode.UpArrow)))
+        {
             float jumpVelocity = 18f;
             rigidbody2D.velocity = Vector2.up * jumpVelocity;
             anim.SetBool("isRunning", false);
             anim.SetBool("isJumping", true);
             //Debug.Log(isGrounded());
             //anim.SetTrigger("attack");
-            
-        }else if(isGrounded()){
+
+        }
+        else if (isGrounded())
+        {
             anim.SetBool("isJumping", false);
         }
 
         //Enables crouching if player is grounded
-        if((isGrounded() && Input.GetKeyDown(KeyCode.S)) || (isGrounded() && Input.GetKeyDown(KeyCode.DownArrow))){
+        if ((isGrounded() && Input.GetKeyDown(KeyCode.S)) || (isGrounded() && Input.GetKeyDown(KeyCode.DownArrow)))
+        {
             anim.SetBool("isCrouching", true);
+
         }
 
-        if((isGrounded() && Input.GetKeyUp(KeyCode.S)) || (isGrounded() && Input.GetKeyUp(KeyCode.DownArrow))){
+        if ((isGrounded() && Input.GetKeyUp(KeyCode.S)) || (isGrounded() && Input.GetKeyUp(KeyCode.DownArrow)))
+        {
             anim.SetBool("isCrouching", false);
         }
 
-        
-        
+        if (anim.GetBool("isCrouching") == true && Input.GetKeyDown("space"))
+        {
 
-        
+            Debug.Log("slide");
+            anim.SetBool("isSliding", true);
+            //transform.position += transform.forward * Time.deltaTime * moveSpeed;
+
+        }
+
+        if (Input.GetKeyUp("space"))
+        {
+            anim.SetBool("isSliding", false);
+        }
+
+
+
+
 
         //flips the character to face the direction they are moving
-        if(moveDirection > 0 && !facingRight){
+        if (moveDirection > 0 && !facingRight)
+        {
             flipCharacter();
             //anim.SetBool("isRunning", true);
-        }else if(moveDirection < 0 && facingRight){
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
             flipCharacter();
             //anim.SetBool("isRunning", true);
         }
 
         //moves the character based on speed and velocity
         rigidbody2D.velocity = new Vector2(moveDirection * moveSpeed, rigidbody2D.velocity.y);
-        if(!isGrounded()){
+        if (!isGrounded())
+        {
             moveSpeed = 0f;
             Debug.Log(moveSpeed);
         }
-        if(moveDirection != 0){
+        if (moveDirection != 0)
+        {
             anim.SetBool("isRunning", true);
-        }else{
+        }
+        else
+        {
             anim.SetBool("isRunning", false);
         }
-        
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if (anim.GetBool("isSliding") == true && isGrounded())
+        {
+
+            float slideVelocity = 8f;
+            if (facingRight)
+            {
+
+                rigidbody2D.velocity = Vector2.right * slideVelocity;
+
+            }
+            else if (!facingRight)
+            {
+                rigidbody2D.velocity = Vector2.left * slideVelocity;
+
+            }
+
+
+            //This method can teleport 
+            //rigidbody2D.MovePosition(transform.forward * moveSpeed);
+
+        }
+        //boxCollider2D.enabled = true;
+
     }
 
     //return true if player is touching the ground
-    private bool isGrounded(){
-        
+    private bool isGrounded()
+    {
+
 
         RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, .1f, layerMask);
-        
+
         return raycastHit2D.collider != null;
-        
+
     }
 
     //method to turn the character or flip the sprite
-    private void flipCharacter(){
-        if(!PlayerHealthManager.playerDead){
+    private void flipCharacter()
+    {
+        if (!PlayerHealthManager.playerDead)
+        {
             facingRight = !facingRight;
             transform.Rotate(0f, 180f, 0f);
         }
